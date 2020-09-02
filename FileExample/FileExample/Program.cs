71d17fd3357085b798190574eab5f1c6f110d7f7
Brandon Pham <Brandonhpham@ou.edu>
2020-09-02 11:36:22 -0500

    created fileexample project

---
+++ FileExample/FileExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace FileExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string filePath = @"C:\Users\Brando\Desktop\Pride and Prejudice by Jane Austen.txt";
+
+            //Reads the entire file into 1 single string
+            //string contentsofFile = File.ReadAllText(filePath);
+
+            var lines = File.ReadAllLines(filePath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string currentLine = lines[i];
+                if (currentLine.Contains("Chapter"))
+                    {
+                    Console.ReadKey();
+                    }
+            }
+
+            //output 5 lines at a time of the file
+            //for (int i = 0; i < lines.Length; i++)
+            // {
+            //  if (i % 5 == 0 && i != 0)
+            // {
+            // Console.ReadKey();
+            // }
+
+            // Console.WriteLine(lines[i]);
+            // }
+
+            //  Console.WriteLine(lines);
+        }
+    }
+}
